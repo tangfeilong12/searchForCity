00999451c764ad262773e55ce7ee41da23a1f5f3
xmgcs <mac@tangfeilong.local>
2015-11-19 01:28:18 +0800

    加了两个文件
    加了两个文件

---
+++ searchForCity/searchForCity/classes/category/UIView+CM.h
@@ -0,0 +1,13 @@
+//
+//  UIView+CM.h
+//  searchForCity
+//
+//  Created by mac on 15/11/19.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (CM)
+
+@end
---
+++ searchForCity/searchForCity/classes/category/UIView+CM.m
@@ -0,0 +1,13 @@
+//
+//  UIView+CM.m
+//  searchForCity
+//
+//  Created by mac on 15/11/19.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import "UIView+CM.h"
+
+@implementation UIView (CM)
+
+@end
